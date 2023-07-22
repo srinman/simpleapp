099ea0021ca20c7e6aa6d0243e4d4aa1db9b4efd
Sridher Manivel <smanivel@microsoft.com>
2023-07-22 17:23:10 +0000

    test

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT", None)
 
 @app.route('/')
 @app.route('/index.html')
