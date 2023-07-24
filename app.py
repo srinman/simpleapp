926db40b087bdb78ec40018d9a106940e2d7db7d
Sridher Manivel <sridhermanivel@sridhers-mbp.lan>
2023-07-23 21:18:31 -0400

    3 versions pushed to acr

--- app.py
+++ app.py
@@ -11,4 +11,8 @@ CORS(app)
 @app.route('/')
 @app.route('/index.html')
 def welcome_page():
-    return render_template('index.html')
+    return render_template('indexv1.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
